51a633ad886ae88c73c840685530dc47d2a0c1a7
Mohamed Salah Dimessi <mohamed.dimessii@gmail.com>
2023-07-10 15:53:07 +0100

    initial commit

---
+++ src/pages/setting/setting.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react';
+import {
+    MenuFoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import {Link, Outlet, useNavigate} from "react-router-dom";
+const { Header, Sider, Content } = Layout;
+
+
+const Setting = () => {
+    const navigate  = useNavigate()
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    return (
+        <Layout>
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="demo-logo-vertical" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['/home']}>
+                    <Menu.Item key="home" icon={<UserOutlined />}>
+                        <Link to="/setting/categories">Home</Link>
+                    </Menu.Item>
+                    <Menu.Item key="aricles" icon={<UserOutlined />}>
+                        <Link to="/setting/articles">About</Link>
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Button
+                        type="text"
+                        icon={<MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Outlet />
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default Setting;
